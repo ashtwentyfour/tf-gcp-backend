0f8236bbd83c3f4b7ceb0acde263dba89b80de1a
Ashwin Menon <ashtwentyfour@Ashwins-MacBook-Air.local>
2023-02-14 18:28:28 -0500

    added outputs

--- tf/main.tf
+++ tf/main.tf
@@ -2,11 +2,12 @@ resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "default" {
-  name          = "tfstate-${random_id.bucket_prefix.hex}"
-  force_destroy = false
-  location      = var.location
-  storage_class = "STANDARD"
+resource "google_storage_bucket" "bucket" {
+  name                     = "tfstate-${random_id.bucket_prefix.hex}"
+  force_destroy            = false
+  location                 = var.location
+  storage_class            = "STANDARD"
+  public_access_prevention = "enforced"
   versioning {
     enabled = true
   }
